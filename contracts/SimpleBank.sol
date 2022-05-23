1f09683eb0ef751413260573c1e540c2b431feb2
Christopher Ferreira <christopher.ferreira@consensys.net>
2022-05-23 11:00:29 +0100

    Changing compiler version

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -48,11 +48,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    fallback() external payable {
-        revert();
-    }
-
-    receive() external payable {
+    function fallback() external payable {
         revert();
     }
 
@@ -103,7 +99,7 @@ contract SimpleBank {
         // 2. Transfer Eth to the sender and decrement the withdrawal amount from
         //    sender's balance
         // 3. Emit the appropriate event for this message
-        address payable to = payable(msg.sender);
+        address payable to = msg.sender;
 
         uint256 currentBalance = balances[msg.sender];
         require(currentBalance >= withdrawAmount);
